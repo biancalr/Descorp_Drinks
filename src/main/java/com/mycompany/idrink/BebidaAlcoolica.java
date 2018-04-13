3f57a0d86cfd305fb28dd78b89d8abddd693ab3c
biancalr <33333728+biancalr@users.noreply.github.com>
2018-04-12 22:47:50 -0300

    Atualizado

---
+++ src/main/java/com/mycompany/idrink/BebidaAlcoolica.java
@@ -0,0 +1,39 @@
+package com.mycompany.idrink;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+
+/**
+ *
+ * @author Bianca
+ */
+@Entity
+@DiscriminatorValue(value = "SIM")
+public class BebidaAlcoolica extends Bebida implements Serializable {
+    
+    @Column(name = "NUM_TEOR_ALCOOL", nullable = false)
+    private Float teor;
+
+    public Float getTeor() {
+        return teor;
+    }
+
+    public void setTeor(Float teor) {
+        this.teor = teor;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder(super.toString());
+        sb.append(" Alcoólica: SIM\n Teor Alcoólico:");
+        sb.append(this.teor);
+        return sb.toString();
+    }
+    
+    
+    
+    
+}
