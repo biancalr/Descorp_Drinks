09f9e632ce2a84fb0b62f93b8aa0eccc52b97b19
biancalr <33333728+biancalr@users.noreply.github.com>
2018-04-12 22:42:35 -0300

    Atualizado

---
+++ src/main/java/com/mycompany/idrink/Cartao.java
@@ -0,0 +1,112 @@
+package com.mycompany.idrink;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author Bianca
+ */
+@Entity
+@Table(name = "tb_cartao")
+public class Cartao implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @OneToOne(mappedBy = "cartao")
+    private Cliente cliente;
+    @Column(name = "TXT_BANDEIRA", length = 30, nullable = false)
+    private String bandeira;
+    @Temporal(TemporalType.DATE)
+    @Column(name = "DT_EXPIRACAO", nullable = false)
+    private Date dataExpiracao;
+    @Column(name = "TXT_NUMERO", length = 50, nullable = false)
+    private String numero;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public String getBandeira() {
+        return bandeira;
+    }
+
+    public void setBandeira(String bandeira) {
+        this.bandeira = bandeira;
+    }
+
+    public Date getDataExpiracao() {
+        return dataExpiracao;
+    }
+
+    public void setDataExpiracao(Date dataExpiracao) {
+        this.dataExpiracao = dataExpiracao;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+    
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Cartao other = (Cartao) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("Cartao: \n");
+        sb.append(" Número:");
+        sb.append(this.numero);
+        sb.append("\n Bandeira:");
+        sb.append(this.bandeira);
+        sb.append("\n Data de Expiração:");
+        sb.append(this.dataExpiracao);
+        sb.append("\n");
+        return sb.toString();
+    }
+    
+}
