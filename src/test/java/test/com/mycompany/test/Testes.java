141529cd90914615a6ba29195f98d3d0158131ff
biancalr <33333728+biancalr@users.noreply.github.com>
2018-04-30 22:23:58 -0300

    Remover Cliente adicionado

--- src/test/java/test/com/mycompany/test/Testes.java
+++ src/test/java/test/com/mycompany/test/Testes.java
@@ -15,7 +15,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import javax.persistence.TypedQuery;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -92,14 +91,14 @@ public class Testes {
         cliente.setEmail("xuxa@gmail.com");
         cliente.setSenha("6666");
         criarEndereco(cliente);
-        //criarCartao(cliente);
+        criarCartao(cliente);
         em.persist(cliente);
         em.flush();
         assertNotNull(cliente.getId());
         logger.log(Level.INFO, "Cliente {0} incluído com sucesso.", cliente);
     }
     
-    public Endereco criarEndereco(Cliente cliente){
+    private Endereco criarEndereco(Cliente cliente){
         Endereco endereco = new Endereco();
         endereco.setCep("50690220");
         endereco.setEstado("Pernambuco");
@@ -107,71 +106,62 @@ public class Testes {
         endereco.setBairro("Iputinga");
         endereco.setLogradouro("Rua Iolanda Rodrigues Sobral");
         endereco.setNumero(550);
+        endereco.setComplemento("Apto. 109");
         cliente.setEndereco(endereco);
         em.flush();
         return endereco;
     }
     
-     /*private Cartao criarCartao() {
+    private void criarCartao(Cliente cliente){
         Cartao cartao = new Cartao();
+        cartao.setBandeira("OUROCARD");
+        cartao.setNumero("18888281889");
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.YEAR, 2019);
+        c.set(Calendar.MONTH, Calendar.AUGUST);
+        c.set(Calendar.DAY_OF_MONTH, 04);
+        cartao.setDataExpiracao(c.getTime());
+        cliente.setCartao(cartao);
+        em.flush();
+    }
+    
+    @Test
+     public void atualizarCartao() {
+        logger.info("Executando t02: Atualizar Cartao");
+        Cliente cliente = em.find(Cliente.class, new Long(4));
+        assertNotNull(cliente.getId());
+        Cartao cartao = new Cartao();
+        cartao.setNumero("120000-100");
         cartao.setBandeira("VISA");
         Calendar c = Calendar.getInstance();
         c.set(Calendar.YEAR, 2020);
-        c.set(Calendar.MONTH, Calendar.JUNE);
+        c.set(Calendar.MONTH, Calendar.JULY);
         c.set(Calendar.DAY_OF_MONTH, 13);
         cartao.setDataExpiracao(c.getTime());
-        cartao.setNumero("120000-100");
+        cliente.setCartao(cartao);
+        em.merge(cartao);
         em.flush();
-        return cartao;
-    }*/
-    /*
+        em.clear();
+        assertNotNull(cartao.getId());
+        logger.log(Level.INFO, "Cartao atualizado com sucesso", cartao);
+        
+    }
+    
     @Test
     public void removerCliente() {
-        logger.info("Executando t02: remover Cliente");
-        TypedQuery<Cliente> query = em.createNamedQuery("Cliente.PorNome", Cliente.class);
-        query.setParameter("nome", "Beltrano Silva");
-        Cliente cliente = query.getSingleResult();
-        assertNotNull(cliente);
+        logger.info("Executando t03: Remover Cliente");
+        Cliente cliente = em.find(Cliente.class, new Long(2));
+        assertNotNull(cliente.getId());
+        cliente = em.merge(cliente);
         em.remove(cliente);
-        em.flush();
-        assertEquals(0, query.getResultList().size());
+        cliente = em.find(Cliente.class, new Long(2));
+        assertNull(cliente);
+        logger.log(Level.INFO, "Cliente removido com sucesso", cliente);
     }
-     */
-     /*
-    @Test
-    public void atualizarCliente() {
-        //Não atualiza endereco nem cartão
-        logger.info("Executando t03: atualizar Cliente");
-        TypedQuery<Cliente> query = em.createNamedQuery("Cliente.PorNome", Cliente.class);
-        query.setParameter("nome", "Beltrano Silva");
-        Cliente cliente = query.getSingleResult();
-        assertNotNull(cliente);
-        cliente.setNome("Josh José");
-        cliente.setTelefone("3356-9694");
-        cliente.setLogin("jojo");
-        cliente.setSenha("55321");
-        em.flush();
-        assertEquals(0, query.getResultList().size());
-    }*/
-    /*
-    public void atualizarEndereco() {
-        //Não atualiza cliente
-        logger.info("Executando t04: atualizar Endereco");
-        TypedQuery<Cliente> query = em.createNamedQuery("Cliente.PorNome", Cliente.class);
-        query.setParameter("nome", "Beltrano Silva");
-        Cliente cliente = query.getSingleResult();
-        Endereco endereco = new Endereco();
-        assertNotNull(cliente);
-        endereco.setCep("35197114");
-        endereco.setEstado("Bahia");
-        endereco.setCidade("Pina");
-        endereco.setBairro("Favela do Bode");
-        endereco.setComplemento("bloco 13");
-        endereco.setLogradouro("Rua Encanta Moça");
-        endereco.setNumero(45);
-        em.flush();
-        assertEquals(0, query.getResultList().size());
-    }
-    */
+    
+     @Test
+     public void removerCartao(){
+         logger.log(Level.INFO, "Executando t04: Remover Cartão");
+     }
     
 }
