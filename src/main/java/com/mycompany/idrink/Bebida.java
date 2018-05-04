f9ab041914859b3e7dfe5edc4ba9482409a7179a
biancalr <33333728+biancalr@users.noreply.github.com>
2018-05-03 21:53:33 -0300

    Atualizado

--- src/main/java/com/mycompany/idrink/Bebida.java
+++ src/main/java/com/mycompany/idrink/Bebida.java
@@ -7,6 +7,7 @@ import javax.persistence.AccessType;
 import javax.persistence.Column;
 import javax.persistence.DiscriminatorColumn;
 import javax.persistence.DiscriminatorType;
+import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
