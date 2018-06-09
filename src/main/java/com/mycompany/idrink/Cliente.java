a80a053072876941b72a03a26703292b05924aa7
biancalr <33333728+biancalr@users.noreply.github.com>
2018-06-09 02:31:15 -0300

    Atualizado

--- src/main/java/com/mycompany/idrink/Cliente.java
+++ src/main/java/com/mycompany/idrink/Cliente.java
@@ -21,7 +21,6 @@ import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.validation.Valid;
-import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotBlank;
