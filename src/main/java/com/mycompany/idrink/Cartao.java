49a8cea7db58eeb876af5a69d092e29973357b52
biancalr <33333728+biancalr@users.noreply.github.com>
2018-06-09 02:25:16 -0300

    Atualizado

--- src/main/java/com/mycompany/idrink/Cartao.java
+++ src/main/java/com/mycompany/idrink/Cartao.java
@@ -11,7 +11,6 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-import javax.validation.constraints.Future;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import org.hibernate.validator.constraints.NotBlank;
